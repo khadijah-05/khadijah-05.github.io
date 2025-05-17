ad9fc08f7ae8b0c20343cce1fef5c23da04571d4
khadijah-05 <khadija786.02005@gmail.com>
2025-05-17 18:10:31 +0400

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", () => {// Wait till to load 
+    const form = document.querySelector(".appointment-form");
+    if (form) {
+      form.classList.add("slide-up");
+    }
+  });
+  
